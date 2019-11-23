4141073db7c9d61049ca65e3a7ec48e218500daa
mcha2884 <57397887+mcha2884@users.noreply.github.com>
2019-11-23 16:35:12 +0900

    ajax #3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,11 @@
 $(function(){ 
   function buildHTML(message){
    if ( message.image ) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
+    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : ""; 
+
+    var html = `<div class="message" data-message-id="${message.id}"> 
+          <div class="upper-message">
+            <div class="upper-message__user-name">
              ${message.user_name}
            </div>
            <div class="upper-message__date">
