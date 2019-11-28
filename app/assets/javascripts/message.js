3e4d466415db0ed7c85d0af85c06a6d1837498cb
mcha2884 <57397887+mcha2884@users.noreply.github.com>
2019-11-29 01:49:54 +0900

    set2 S#

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -35,7 +35,7 @@ $(function(){
       contentType: false,
     })
     .done(function(data){
-      var html = buildHTML(data);
+      let html = buildHTML(data);
       $('.messages').append(html);   
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
       $('form')[0].reset();
